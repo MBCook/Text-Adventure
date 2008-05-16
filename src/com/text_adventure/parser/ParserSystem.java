044b92a92ff60d7a4a0aeecc4eef79db8752abcc
Michael Cook <foobarsoft@foobarsoft.com>
2008-05-16 19:05:36 +0000

    Starting of parsing out a sentence into ParserTokens

--- src/com/text_adventure/parser/ParserSystem.java
+++ src/com/text_adventure/parser/ParserSystem.java
@@ -1,8 +1,12 @@
 package com.text_adventure.parser;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import com.text_adventure.exception.InvalidGrammarException;
+import com.text_adventure.exception.UnknownObjectException;
 import com.text_adventure.parser.special_words.GameDirectionWord;
 import com.text_adventure.parser.special_words.GamePrepositionWord;
 import com.text_adventure.parser.special_words.GameSpecialWord;
@@ -16,6 +20,7 @@ import com.text_adventure.parser.verbs.TakeVerb;
 import com.text_adventure.parser.verbs.ThrowVerb;
 import com.text_adventure.parser.verbs.UseVerb;
 import com.text_adventure.parser.verbs.WalkVerb;
+import com.text_adventure.world_objects.GameWorld;
 import com.text_adventure.world_objects.WorldDirection;
 
 /**
@@ -99,5 +104,80 @@ public class ParserSystem {
 	 */
 	public GameSpecialWord getSpecialWordFromWord(String word) {
 		return wordToSpecialWordMap.get(word);
+	}
+	
+	/**
+	 * Take the given sentence and turn it into a list of parser tokens
+	 * @param world The game world we're operating in
+	 * @param sentence The sentence to parse
+	 * @return A list of tokens
+	 */
+	public List<ParserToken> parseSentence(GameWorld world, String sentence)
+													throws InvalidGrammarException {
+		List<ParserToken> result = new ArrayList<ParserToken>();
+		
+		// First, sanity check the sentence
+		
+		if ((sentence == null) || (sentence.trim().length() == 0)) {
+			throw new InvalidGrammarException("Given an empty sentence");
+		}
+		
+		// OK, split it into words
+		
+		String[] words = ParserHelper.splitStringIntoWords(sentence.toLowerCase());
+		
+		// Now go through each word looking for the correct object
+		
+		for (String word : words) {
+			if ((word != null) && (word.trim().length() > 0)) {
+				// We've got a word. Find out what it is
+				
+				word = word.trim();
+				
+				// First, find out if it's a verb
+				
+				ParserToken token = getVerbFromWord(word);
+				
+				if (token != null) {
+					// 'Twas a verb, save it and move on
+					
+					result.add(token);
+					
+					continue;
+				}
+				
+				// Maybe it's a special word
+				
+				token = getSpecialWordFromWord(word);
+				
+				if (token != null) {
+					// 'Twas a special word, save it and move on
+					
+					result.add(token);
+					
+					continue;
+				}
+				
+				// OK. It better be a game object
+				
+				token = world.getObjectByName(word);
+				
+				if (token != null) {
+					// It was an object in the game, save it and move on
+					
+					result.add(token);
+					
+					continue;
+				}
+				
+				// We have no idea what this word is. Throw an exception
+				
+				throw new InvalidGrammarException("Unknown word " + word);
+			}
+		}
+		
+		// That's all. If we got here we have the sentence
+		
+		return result;
 	}
 }
